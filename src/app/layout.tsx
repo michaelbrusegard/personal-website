96170f89567b4d70dd8287808a7878e753080c33
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2023-08-02 21:36:27 +0200

    fix: :bug: updated favicon ico to root

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,9 +13,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <link rel='apple-touch-icon' sizes='180x180' href='/icons/favicon/apple-touch-icon.png' />
         <link rel='icon' type='image/png' sizes='32x32' href='/icons/favicon/favicon-32x32.png' />
         <link rel='icon' type='image/png' sizes='16x16' href='/icons/favicon/favicon-16x16.png' />
-        <link rel='manifest' href='/icons/favicon/site.webmanifest' />
         <link rel='mask-icon' href='/icons/favicon/safari-pinned-tab.svg' color='#000000' />
-        <meta name='msapplication-TileColor' content='#DA532C' />
         <meta name='theme-color' content='#FFFFFF' />
       </head>
       <body className='relative bg-background font-body text-text'>
