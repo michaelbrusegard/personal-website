ba3e155a12f4892537447008db9254e2d3f0966f
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2024-12-24 04:54:25 +0100

    fix: favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,20 +2,37 @@ import Nav from '../components/nav';
 
 export const metadata = {
   title: 'Michael | Portifolio',
+  description: 'My first website built with React. Showcasing some of my experience and projects.',
+  icons: [
+    {
+      rel: 'icon',
+      type: 'image/x-icon',
+      url: '/favicon/favicon.ico',
+    },
+    {
+      rel: 'apple-touch-icon',
+      sizes: '180x180',
+      url: '/favicon/apple-touch-icon.png',
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '32x32',
+      url: '/favicon/favicon-32x32.png',
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '16x16',
+      url: '/favicon/favicon-16x16.png',
+    },
+    { rel: 'manifest', url: '/favicon/site.webmanifest' },
+  ],
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang='en'>
-      <head>
-        <link rel='icon' type='image/svg+xml' href='/icons/favicon/favicon.svg' />
-        <link rel='icon' type='image/png' href='/icons/favicon/favicon.png' />
-        <link rel='apple-touch-icon' sizes='180x180' href='/apple-touch-icon.png' />
-        <link rel='icon' type='image/png' sizes='32x32' href='/icons/favicon/favicon-32x32.png' />
-        <link rel='icon' type='image/png' sizes='16x16' href='/icons/favicon/favicon-16x16.png' />
-        <link rel='mask-icon' href='/icons/favicon/safari-pinned-tab.svg' color='#000000' />
-        <meta name='theme-color' content='#FFFFFF' />
-      </head>
+    <html lang='en' dir='ltr' className='h-full w-full'>
       <body className='relative bg-background font-body text-text'>
         <Nav />
         {children}
