2cb6253096de764839ada0f5a1cd68c107960f00
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-01-10 11:21:21 +0100

    fix: small screen not showing works bug

--- src/components/layout/SectionHOC.tsx
+++ src/components/layout/SectionHOC.tsx
@@ -12,7 +12,7 @@ function SectionHOC<T extends object>(Component: ComponentType<T>, id: string) {
         variants={staggerContainer()}
         initial='hidden'
         whileInView='show'
-        viewport={{ once: true, amount: 0.15 }}
+        viewport={{ once: true, margin: '0px 0px -200px 0px' }}
         className='relative z-10 mx-auto max-w-7xl scroll-mt-24 p-6 sm:p-11 md:p-16'
       >
         <Component {...(props as T)} />
